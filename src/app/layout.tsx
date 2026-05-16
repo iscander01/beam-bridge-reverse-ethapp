db959369f593a25958f93d0bee4062508e2ef124
bro <bro@beam-mw.com>
2026-05-17 01:25:20 +0200

    feat: footer update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className="overscroll-none">
+      <body className="overscroll-none">
         <Providers>
           <Shell>{children}</Shell>
         </Providers>
